563558c3ef8a07309c40fb3f6bcb2907c57bf538
sonalikpi <sonali.jadhav@kpipartners.com>
2024-08-23 20:46:00 +0530

    Update dummy.sql

--- snowflake-demo-notebooks-main/snowflake-demo-notebooks-main/dummy.sql
+++ snowflake-demo-notebooks-main/snowflake-demo-notebooks-main/dummy.sql
@@ -1,1 +1,5 @@
-create table dummy_tbl (col varchar);
+create or table dummy_tbl (col varchar);
+create or table dummy_tbl1 (col varchar);
+create or table dummy_tbl2 (col varchar);
+create or table dummy_tbl3 (col varchar);
+create or table dummy_tbl4 (col varchar);
